5b0b8ee44c94a8913384d80eb15a77f41b939531
Batyr <b.bobetayev.97@gmail.com>
2020-06-04 19:02:48 +0200

    first

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+// import loadable from 'react-loadable'
+
+// export default [
+//   {
+//     path: '/',
+//     exact: true,
+//     component: loadable({
+//       loader: () => import('./Views/Main')
+//     })
+//   },
+//   {
+//     path: '/login',
+//     exact: true,
+//     component: loadable({
+//       loader: () => import('./Views/Login')
+//     })
+//   }
+// ]
